1cc10ac9b220395518d8bfa293b486920cd4c079
KKMalysa <k.malysa010@gmail.com>
2026-03-29 13:13:30 +0200

    Initial commit

---
+++ src/main/java/com/kkmalysa/clamtransactions/ClamTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.kkmalysa.clamtransactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClamTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClamTransactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kkmalysa/clamtransactions/ClamTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkmalysa.clamtransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClamTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
